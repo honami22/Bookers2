488bfa7366709587e1fc2f7e5fc4f7fe0a427dd0
EC2 Default User <ec2-user@ip-172-31-2-209.ap-northeast-1.compute.internal>
2023-05-31 12:00:17 +0000

    [modo] 課題3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
+  # get 'post_image/new'
+  # get 'post_image/index'
+  # get 'post_image/show'
+
+  resources :post_image, only: [:show, :edit,:index]
+  # get 'books/new'
+#   get 'books/index'
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get 'books/show'
-  get 'books/edit'
+  get 'books/:id/new' => 'books#new', as: 'new_book'
+   get 'books/:id' => 'books#show', as: 'show_book'
+   get 'books/:id/edit'  => 'books#edit' , as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+# resources :book, only: [:show, :edit,:update,:destroy,:create]
+
   devise_for :users
  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_28_132049) do
+ActiveRecord::Schema.define(version: 2023_05_28_161517) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
